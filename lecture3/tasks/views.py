1c470f932624becc639dba63d2325938b529694c
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-09 21:02:38 +0200

    used django to make forms instead of hard coding in html

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 
@@ -10,4 +15,6 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
