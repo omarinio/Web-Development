27295e5a76192c36ee332f7bace5603d0e8e9943
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-09 21:13:09 +0200

    reverse url

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 
@@ -20,6 +21,7 @@ def add(request):
         if form.is_valid():
             task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
