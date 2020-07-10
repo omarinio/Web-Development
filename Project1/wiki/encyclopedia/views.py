fb8e9633fd90b58120d9b8577d0ef8161d94cc47
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-10 16:42:12 +0200

    2nd project, url displays correct page and if doesn't exist, error shown

--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -8,3 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    entry = util.get_entry(title)
+
+    if entry:
+        return render(request, "encyclopedia/title.html", {
+            "entry": util.get_entry(title),
+            "title": title
+        })
+
+    else:
+        return render(request, "encyclopedia/error.html")
+
+    
