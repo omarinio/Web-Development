e70f7d363e53d3ef22a56be07ddc27952414326e
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-16 20:48:48 +0200

    ability to add new passengers to flight from non passengers

--- lecture4/airline/flights/views.py
+++ lecture4/airline/flights/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Flight
+from django.shortcuts import render, redirect
+from .models import Flight, Passenger
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,13 @@ def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
-   
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk = flight_id)
+        passenger = Passenger.objects.get(pk = int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return redirect(reverse("flight", args=(flight.id,)))
