1777ddea9564be9481f12acdd3c25e1141cf204e
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-21 16:11:52 +0200

    changed comment model to feature time of comment creation

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -39,6 +39,7 @@ class Comment(models.Model):
     comment = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Commenter")
     item = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="ListComment")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user} {self.item} {self.comment}"
