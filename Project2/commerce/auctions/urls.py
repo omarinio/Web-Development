c875a3f31b4a045d8856936b892eadef69563d88
Omar Suleiman <jb18940@bristol.ac.uk>
2020-07-22 14:46:15 +0200

    trying to call a method with @login_required redirects you to signin

--- Project2/commerce/auctions/urls.py
+++ Project2/commerce/auctions/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create"),
     path("listing/<int:id>", views.listing, name="listing"),
